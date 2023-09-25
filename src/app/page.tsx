b67105b81e0092033dc022c4beecbba438ac5ed1
Karl J. Overå <k@rl.run>
2023-09-25 12:59:37 +0200

    husky and lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home(): ReactElement {
         <main className="flex items-center justify-between p-24">
             <Heading size="xlarge">Statistikk for sykmelder</Heading>
             <Button>Does style work?</Button>
-            <div className="bg-bg-subtle p-">Tailwind test</div>
+            <div className="bg-bg-subtle p-4">Tailwind test</div>
+            <div className="bg-bg-subtle p-8">Tailwind test 2</div>
         </main>
     )
 }

372659e5828b55b3578f36a49a6dd61e637bf1ef
Karl J. Overå <k@rl.run>
2023-11-15 16:57:53 +0100

    add script to sync changesets from sykmelder-statistikk-kafka

--- src/db/dev/dev-db.ts
+++ src/db/dev/dev-db.ts
@@ -1,14 +1,13 @@
 import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@testcontainers/postgresql'
 import { lazyNextleton } from 'nextleton'
 import { logger } from '@navikt/next-logger'
-import { PoolClient } from 'pg'
 
 export const devDatabase: () => Promise<StartedPostgreSqlContainer> = lazyNextleton('dev-testcontainers', async () => {
     logger.info('Starting dev database')
 
     return new PostgreSqlContainer()
         .withDatabase('statistikk')
-        .withUsername('dev-user')
+        .withUsername('sykmelder-statistikk-next')
         .withPassword('dev-password')
         .start()
         .then((container) => {
@@ -17,23 +16,3 @@ export const devDatabase: () => Promise<StartedPostgreSqlContainer> = lazyNextle
             return container
         })
 })
-
-export async function seedTestDatabase(client: PoolClient): Promise<void> {
-    await client.query(`
-        CREATE TABLE IF NOT EXISTS test_table
-        (
-            id   SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL
-        );
-    `)
-
-    await client.query(`
-        INSERT INTO test_table (name)
-        VALUES ('test');
-        INSERT INTO test_table (name)
-        VALUES ('test');
-        INSERT INTO test_table (name)
-        VALUES ('test');
-
-    `)
-}
